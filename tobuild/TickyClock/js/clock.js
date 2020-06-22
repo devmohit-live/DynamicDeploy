fa4b6892326d83539d0b174865721063c72f34dd
mohit <mohitsingh.it5@gmail.com>
2020-06-22 19:45:45 +0530

    Path updated

--- tobuild/TickyClock/js/clock.js
+++ tobuild/TickyClock/js/clock.js
@@ -1,29 +1,28 @@
 var d, h, m, s, clock, color;
 
-function cloky(){
-    d = new Date();
-    h = d.getHours();
-    m = d.getMinutes();
-    s = d.getSeconds();
+function cloky() {
+  d = new Date();
+  h = d.getHours();
+  m = d.getMinutes();
+  s = d.getSeconds();
 
+  if (h <= 9) {
+    h = "0" + h;
+  }
+  if (m <= 9) {
+    m = "0" + m;
+  }
+  if (s <= 9) {
+    s = "0" + s;
+  }
 
-if ( h <= 9){
-    h = '0' + h;
-}
-if ( m <= 9){
-    m = '0' + m;
-}
-if ( s <= 9){
-    s = '0' + s;
-}
+  clock = h + ":" + m + ":" + s;
+  color = "#" + h + m + s;
 
-clock = h + ":" + m + ":" + s;
-color = "#" + h + m + s;
+  document.getElementById("clock").innerHTML = clock;
+  document.body.style.background = color;
 
-document.getElementById("clock").innerHTML = clock
-document.body.style.background = color;
-
-setTimeout(cloky, 1000);
+  setTimeout(cloky, 1000);
 }
 
 cloky();
